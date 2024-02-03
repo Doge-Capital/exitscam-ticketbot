abc13f8eef8355627f769de3227ffd49129cceb5
eibelsalil <eibelsm@gmail.com>
2024-02-03 11:35:43 +0530

    auto detect game

--- index.ts
+++ index.ts
@@ -14,11 +14,11 @@ config();
 const devWallet = Uint8Array.from(JSON.parse(process.env.SCRIPT_KEYPAIR!));
 const devWalletKey = Keypair.fromSecretKey(devWallet);
 
-let gameAccount = new PublicKey("2krNPf5tHiSYpSY6EwHYpVyxwAM5ovrYwuj4sTtUYWJg");
+// let gameAccount = new PublicKey("2krNPf5tHiSYpSY6EwHYpVyxwAM5ovrYwuj4sTtUYWJg");
 
-let vaultAccount = new PublicKey(
-  "HUqbrRwSfgGS9VtUKTHgRs2Z9n5kWHHKKyGf8um3uhMR"
-);
+// let vaultAccount = new PublicKey(
+//   "HUqbrRwSfgGS9VtUKTHgRs2Z9n5kWHHKKyGf8um3uhMR"
+// );
 
 const main = async () => {
   while (true) {
@@ -30,10 +30,10 @@ const main = async () => {
       );
       const program = new Program(IDL, programId, provider);
 
-      // let [vaultAccount] = PublicKey.findProgramAddressSync(
-      //   [encode(VAULT_SEED_V2), authority.toBuffer()],
-      //   program.programId,
-      // );
+      let [vaultAccount] = PublicKey.findProgramAddressSync(
+        [encode(VAULT_SEED_V2), authority.toBuffer()],
+        program.programId,
+      );
 
       let vaultInfo = await program.account.vaultAccountV2.fetch(
         vaultAccount,
@@ -53,40 +53,40 @@ const main = async () => {
         null
       );
 
-      // let gameAccount;
+      let gameAccount;
 
-      // if (currGameId !== null) {
-      //   const buffer = Buffer.allocUnsafe(2);
-      //   buffer.writeUInt16LE(currGameId, 0);
+      if (currGameId !== null) {
+        const buffer = Buffer.allocUnsafe(2);
+        buffer.writeUInt16LE(currGameId, 0);
 
-      //   [gameAccount] = PublicKey.findProgramAddressSync(
-      //     [encode(GAME_SEED_V2), authority.toBuffer(), buffer],
-      //     program.programId,
-      //   );
-      // } else if (!gameAccount) return null;
+        [gameAccount] = PublicKey.findProgramAddressSync(
+          [encode(GAME_SEED_V2), authority.toBuffer(), buffer],
+          program.programId,
+        );
+      } else if (!gameAccount) return null;
 
       let gameInfo = await program.account.gameAccountV2.fetchNullable(
         gameAccount,
         "recent"
       );
 
-      let ticketNum = 0;
+      // let ticketNum = 0;
 
-      let ticketStartNum =
-        gameInfo!.totalTickets.toNumber() -
-        gameInfo!.constTickets.toNumber() +
-        1;
+      // let ticketStartNum =
+      //   gameInfo!.totalTickets.toNumber() -
+      //   gameInfo!.constTickets.toNumber() +
+      //   1;
 
-      let i = 0;
-      let flag = true;
+      // let i = 0;
+      // let flag = true;
 
-      while (flag) {
-        if (getTotalTicketPrice(ticketStartNum, i) > 0.5) {
-          ticketNum = i;
-          flag = false;
-        }
-        ++i;
-      }
+      // while (flag) {
+      //   if (getTotalTicketPrice(ticketStartNum, i) > 0.5) {
+      //     ticketNum = i;
+      //     flag = false;
+      //   }
+      //   ++i;
+      // }
 
       let timeLeft = gameInfo!.endTime.toNumber() - Date.now() / 1000;
 
@@ -96,7 +96,7 @@ const main = async () => {
         const { transactions, blockhash } = await buyTicketTransactions(
           walletId,
           "dragon",
-          ticketNum,
+          1,
           buyTicketAccounts,
           program
         );
